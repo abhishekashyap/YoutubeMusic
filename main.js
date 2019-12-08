cfa8fee235b71cbb038d667e1e9d9c49b5dab13b
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2019-12-08 21:45:44 +0530

    Added screenshots' resources

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function createWindow() {
   mainWindow.loadURL("http://music.youtube.com/");
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on("closed", function() {
